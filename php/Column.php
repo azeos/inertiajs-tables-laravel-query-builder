ee2a3fe44cea88776763c85ce4afe67f04e365d7
Matías Minevitz <matias.minevitz@gmail.com>
2024-03-30 14:28:41 -0300

    Update Column.php

--- php/Column.php
+++ php/Column.php
@@ -9,10 +9,13 @@ class Column implements Arrayable
     public function __construct(
         public string $key,
         public string $label,
+        public string|null $abbr,
         public bool $canBeHidden,
         public bool $hidden,
         public bool $sortable,
-        public bool|string $sorted
+        public bool|string $sorted,
+        public string|null $tdClasses,
+        public string|null $thClasses
     ) {
     }
 
@@ -21,10 +24,13 @@ class Column implements Arrayable
         return [
             'key'           => $this->key,
             'label'         => $this->label,
+            'abbr'          => $this->abbr,
             'can_be_hidden' => $this->canBeHidden,
             'hidden'        => $this->hidden,
             'sortable'      => $this->sortable,
             'sorted'        => $this->sorted,
+            'tdClasses'     => $this->tdClasses,
+            'thClasses'     => $this->thClasses,
         ];
     }
 }
